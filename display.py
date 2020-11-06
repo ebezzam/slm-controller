54ff2b85f7c616eab27346048b6bb5633fdec8d9
Eric B <eric.bezzam@epfl.ch>
2020-11-06 15:34:29 +0100

    Add optional to rescale and crop image when loading.

--- display.py
+++ display.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image, ImageDraw
 
 
-def load_image(fname):
+def load_image(fname, width=None, height=None):
     """
     Load an image.
 
@@ -12,6 +12,8 @@ def load_image(fname):
     ----------
     fname : str, path-like
         Valid image file (i.e. JPG, PNG, BMP, TIFF, etc.)
+    width : int, optional
+    height : int, optional
 
     Returns
     -------
@@ -19,7 +21,31 @@ def load_image(fname):
         ([N_channel,] N_height, N_width) image.
         Output dtype is format-dependent.
     """
-    I_p = Image.open(fname, mode='r')
+    I_p = Image.open(fname, mode="r")
+
+    # rescale and resize if need be
+    if width is not None or height is not None:
+        if width is None:
+            width = I_p.width
+        if height is None:
+            height = I_p.height
+
+        image_ratio = I_p.width / I_p.height
+        screen_ratio = width / height
+        if screen_ratio < image_ratio:
+            scaled_width = I_p.width * height // I_p.height
+            scaled_height = height
+        else:
+            scaled_width = width
+            scaled_height = I_p.height * width // I_p.width
+        I_p = I_p.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+        # crop and center
+        x = scaled_width // 2 - width // 2
+        y = scaled_height // 2 - height // 2
+        I_p = I_p.crop((x, y, x + width, y + height))
+
+    # re-order dimensions
     I = np.asarray(I_p)  # (N_height, N_width [, N_channel])
     if I.ndim > 2:
         I = I.transpose(2, 0, 1)
