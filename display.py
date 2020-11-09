757e4af5e93260fdcc06020780e2a73631adb8a1
Eric B <eric.bezzam@epfl.ch>
2020-11-09 14:23:30 +0100

    Create separate util file, add option for keeping original aspect ratio.

--- display.py
+++ display.py
@@ -4,73 +4,6 @@ import numpy as np
 from PIL import Image, ImageDraw
 
 
-def load_image(fname, output_shape=None):
-    """
-    Load an image.
-
-    Parameters
-    ----------
-    fname : str, path-like
-        Valid image file (i.e. JPG, PNG, BMP, TIFF, etc.)
-    output_shape : tuple(int)
-        [Optional] rescale image to (height, width) pixels.
-
-    Returns
-    -------
-    I : :py:class:`~numpy.ndarray`
-        ([N_channel,] N_height, N_width) image.
-        Output dtype is format-dependent.
-    """
-    I_p = Image.open(fname, mode="r")
-
-    # rescale and resize if need be
-    if output_shape is not None:
-        height = output_shape[0]
-        width = output_shape[1]
-        image_ratio = I_p.width / I_p.height
-        screen_ratio = width / height
-        if screen_ratio < image_ratio:
-            scaled_width = I_p.width * height // I_p.height
-            scaled_height = height
-        else:
-            scaled_width = width
-            scaled_height = I_p.height * width // I_p.width
-        I_p = I_p.resize((scaled_width, scaled_height), Image.BICUBIC)
-
-        # crop and center
-        x = scaled_width // 2 - width // 2
-        y = scaled_height // 2 - height // 2
-        I_p = I_p.crop((x, y, x + width, y + height))
-
-    # re-order dimensions
-    I = np.asarray(I_p)  # (N_height, N_width [, N_channel])
-    if I.ndim > 2:
-        I = I.transpose(2, 0, 1)
-    return I
-
-
-def save_image(I, fname):
-    """
-    Save image to a file.
-
-    Parameters
-    ----------
-    I : :py:class:`~numpy.ndarray`
-        (N_channel, N_height, N_width) image.
-    """
-    I_max = I.max()
-    I_max = 1 if np.isclose(I_max, 0) else I_max
-
-    I_f = I / I_max  # float64
-    I_u = np.uint8(255 * I_f)  # uint8
-
-    if I.ndim == 3:
-        I_u = I_u.transpose(1, 2, 0)
-
-    I_p = Image.fromarray(I_u)
-    I_p.save(fname)
-
-
 class Display:
     def __init__(self):
         pass
@@ -123,7 +56,27 @@ class RGBDisplay(Display):
         rotation=90,
         baudrate=24000000,
     ):
+        """
+        Object to display images on the Adafruit 1.8" TFT Display Breakout with a Raspberry Pi:
+        https://learn.adafruit.com/1-8-tft-display/overview
+
+        Parameters
+        ----------
+        cs_pin : :py:class:`~adafruit_blinka.microcontroller.generic_linux.periphery_pin.Pin`
+            Raspberry Pi pin connected to TFT_CS pin on the display breakout.
+        dc_pin : :py:class:`~adafruit_blinka.microcontroller.generic_linux.periphery_pin.Pin`
+            Raspberry Pi pin connected to D/C pin on the display breakout.
+        reset_pin : :py:class:`~adafruit_blinka.microcontroller.generic_linux.periphery_pin.Pin`
+            Raspberry Pi pin connected to RESET pin on the display breakout.
+        rotation : 0, 90, 180, or 270
+            Rotation of image on the display.
+        baudrate : int
+            Baud rate
+        """
         super().__init__()
+
+        if rotation not in (0, 90, 180, 270):
+            raise ValueError("Rotation must be 0/90/180/270")
 
         try:
             import board
@@ -162,6 +115,9 @@ class RGBDisplay(Display):
             raise IOError("Failed to load display.")
 
     def clear(self):
+        """
+        Clear display.
+        """
         I = Image.new("RGB", (self.width, self.height))
 
         # Get drawing object to draw on image.
