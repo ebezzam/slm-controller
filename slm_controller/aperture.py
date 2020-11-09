32ef73be7c79ed0fc23e758534a4118f857bd7f4
Eric B <eric.bezzam@epfl.ch>
2020-11-09 17:45:15 +0100

    Add abstract _compute_mask method.

--- slm_controller/aperture.py
+++ slm_controller/aperture.py
@@ -1,5 +1,4 @@
 import abc
-
 import numpy as np
 
 
@@ -11,11 +10,12 @@ class DigitalAperture:
         Parameters
         ----------
         slm_dim : tuple(int)
-            Dimensions (height x width) of the spatial light modulator.
+            Dimensions (height, width) of the spatial light modulator.
         """
         assert slm_dim[0] > 0
         assert slm_dim[1] > 1
         self._slm_dim = slm_dim
+        self._mask = None
 
     @property
     def n_slm_pixels(self):
@@ -24,9 +24,13 @@ class DigitalAperture:
     @property
     def mask(self):
         """
-        Return mask as an :py:class:`~numpy.ndarray` object.
+        Return mask as an :py:class:`~numpy.ndarray` object with shape (3, height, width).
         """
         return self._mask
+
+    @abc.abstractmethod
+    def _compute_mask(self):
+        pass
 
     def plot(self):
 
@@ -56,9 +60,9 @@ class RectAperture(DigitalAperture):
         Parameters
         ----------
         apert_dim : tuple(int)
-            Dimensions (height x width) of aperture.
+            Dimensions (height, width) of aperture.
         slm_dim : tuple(int)
-            Dimensions (height x width) of the spatial light modulator (SLM).
+            Dimensions (height, width) of the spatial light modulator (SLM).
         center : tuple(int)
             [Optional] center of aperture along (SLM) coordinates, indexing starts in top-left
             corner. Default is to place center of aperture at center of SLM.
@@ -77,20 +81,27 @@ class RectAperture(DigitalAperture):
             assert (
                 0 <= center[1] < slm_dim[1]
             ), f"Center {center} must lie within SLM dimensions {slm_dim}."
+        self._center = center
+        self._compute_mask()
+
+    def _compute_mask(self):
 
         # determine position of aperture
-        top_left = (center[0] - apert_dim[0] // 2, center[1] - apert_dim[1] // 2)
-        bottom_right = (top_left[0] + apert_dim[0], top_left[1] + apert_dim[1])
+        top_left = (
+            self._center[0] - self._apert_dim[0] // 2,
+            self._center[1] - self._apert_dim[1] // 2,
+        )
+        bottom_right = (top_left[0] + self._apert_dim[0], top_left[1] + self._apert_dim[1])
         if (
             top_left[0] < 0
             or top_left[1] < 0
-            or bottom_right[0] >= slm_dim[0]
-            or bottom_right[1] >= slm_dim[1]
+            or bottom_right[0] >= self._slm_dim[0]
+            or bottom_right[1] >= self._slm_dim[1]
         ):
             raise ValueError(
                 f"Aperture ({top_left[0]}:{bottom_right[0]}, "
                 f"{top_left[1]}:{bottom_right[1]}) extends past valid "
-                f"SLM indices (0:{slm_dim[0] - 1}, 0:{slm_dim[1] - 1})"
+                f"SLM indices (0:{self._slm_dim[0] - 1}, 0:{self._slm_dim[1] - 1})"
             )
 
         # create mask
