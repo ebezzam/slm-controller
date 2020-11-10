faff43526dd913dc1ade86bf4a307c6d752bfe4a
Eric B <eric.bezzam@epfl.ch>
2020-11-10 15:42:44 +0100

    Add hardware config file.

---
+++ slm_controller/hardware.py
@@ -0,0 +1,8 @@
+devices = {
+    # 1.8'' RGB display by Adafruit: https://learn.adafruit.com/1-8-tft-display/overview
+    # datasheet: https://cdn-shop.adafruit.co3m/datasheets/JD-T1800.pdf
+    "adafruit_1p8_tft_rgb": {
+        "pixel_shape": (0.18e-3, 0.18e-3),
+        "slm_dim": (128, 160),
+    }
+}
