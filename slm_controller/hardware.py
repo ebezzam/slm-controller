7e08c99fff491397664bd1082d6a10b9af40ac46
Eric B <eric.bezzam@epfl.ch>
2020-11-10 17:17:07 +0100

    Update hardware config file.

--- slm_controller/hardware.py
+++ slm_controller/hardware.py
@@ -1,8 +1,24 @@
+class DeviceOptions:
+    ADAFRUIT_RGB = "adafruit_1p8_tft_rgb"
+    ADAFRUIT_GRAYSCALE = "adafruit_1p3_lcd_gray"
+
+
+class DeviceParam:
+    PIXEL_SHAPE = "pixel_shape"
+    SLM_DIM = "slm_dim"
+
+
 devices = {
     # 1.8'' RGB display by Adafruit: https://learn.adafruit.com/1-8-tft-display/overview
-    # datasheet: https://cdn-shop.adafruit.co3m/datasheets/JD-T1800.pdf
-    "adafruit_1p8_tft_rgb": {
-        "pixel_shape": (0.18e-3, 0.18e-3),
-        "slm_dim": (128, 160),
-    }
+    # datasheet: https://cdn-shop.adafruit.com/datasheets/JD-T1800.pdf
+    DeviceOptions.ADAFRUIT_RGB: {
+        DeviceParam.PIXEL_SHAPE: (0.18e-3, 0.18e-3),
+        DeviceParam.SLM_DIM: (128, 160),
+    },
+    # 1.3'' monochrome display by Adafruit: https://learn.adafruit.com/adafruit-sharp-memory-display-breakout
+    # datasheet: https://cdn-shop.adafruit.com/product-files/3502/Data+sheet.pdf
+    DeviceOptions.ADAFRUIT_GRAYSCALE: {
+        DeviceParam.PIXEL_SHAPE: (0.145e-3, 0.145e-3),
+        DeviceParam.SLM_DIM: (144, 168),
+    },
 }
