e4cf6357d6e5c90e8fc631a626c734e7c9a5a7c0
Eric B <eric.bezzam@epfl.ch>
2020-11-18 15:33:13 +0100

    Add option to display monochrome image.

--- examples/monochrome_display.py
+++ examples/monochrome_display.py
@@ -9,20 +9,27 @@ from slm_controller import display, util
 
 @click.command()
 @click.option("--file_path", type=str, default=None)
+@click.option("--threshold", type=int, default=0.5)
 @click.option("--not_original_ratio", is_flag=True)
-def monochrome_display_example(file_path, not_original_ratio):
+def monochrome_display_example(file_path, threshold, not_original_ratio):
 
     # instantiate display object
     D = display.MonochromeDisplay()
 
     # prepare image data
     if file_path is not None:
-        raise NotImplementedError
+        keep_aspect_ratio = not not_original_ratio
+        image = util.load_image(
+            file_path, output_shape=D.shape, keep_aspect_ratio=keep_aspect_ratio
+        )
+        image = image / 255.0
+
     else:
         # random mask
-        P = 0.5
         image = np.random.rand(*D.shape)
-        image = image < P
+
+    # make binary image
+    image = image < threshold
 
     # display
     D.imshow(image)
