40f9dd1eddf72f840af44573c422859bb2fb21cf
Eric B <eric.bezzam@epfl.ch>
2020-11-23 19:15:00 +0100

    FIx default for center.

--- examples/set_aperture.py
+++ examples/set_aperture.py
@@ -69,8 +69,10 @@ def set_aperture(shape, n_cells, rect_shape, center, vertical, device):
             print("Aperture shape : horizontal line")
     else:
         print(f"Aperture shape : {shape}")
-    if center is not None:
+    if len(center) > 0:
         center = (center[0] * cell_dim[0], center[1] * cell_dim[1])
+    else:
+        center = None
 
     # create aperture mask
     ap = None
